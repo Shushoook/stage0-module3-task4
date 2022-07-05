5322c26ae18bbb7688a02bf48c62d0b381ed4654
Shushoook <shushoook666@gmail.com>
2022-07-05 10:15:27 +0400

    stage0-module3-task4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,10 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 4;
-
-        double result = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
-        System.out.println(result);
+        double a = 1.0;
+        double b = 2.0;
+        double y = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+        System.out.println(y);
     }
 }
